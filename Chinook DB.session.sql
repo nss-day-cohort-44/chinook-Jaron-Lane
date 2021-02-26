51ccd0be5012d757e37d313be3f74c3222b1d268
Jaron-Lane <jaronwohlwend@icloud.com>
2021-02-26 11:27:05 -0600

    Problem 4

--- Chinook DB.session.sql
+++ Chinook DB.session.sql
@@ -24,3 +24,8 @@ JOIN Invoice
 WHERE Country = "Brazil";
 
 
+-- sales_agents.sql: Provide a query showing only the Employees who are Sales Agents
+
+SELECT *
+FROM Employee
+WHERE Title LIKE "%Agent%"
