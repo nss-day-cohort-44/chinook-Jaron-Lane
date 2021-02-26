c7ca3641a2c91ca6be1876756adc7bda4104c016
Jaron-Lane <jaronwohlwend@icloud.com>
2021-02-26 11:09:11 -0600

    Problem 3

--- Chinook DB.session.sql
+++ Chinook DB.session.sql
@@ -19,4 +19,8 @@ WHERE Country = "Brazil";
 
 SELECT FirstName, LastName, InvoiceId, InvoiceDate, BillingCountry
 FROM Customer 
+JOIN Invoice
+    ON Customer.CustomerId = Invoice.CustomerId
 WHERE Country = "Brazil";
+
+
