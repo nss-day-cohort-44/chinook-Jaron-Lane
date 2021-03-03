21f92ac5616911c74d209ec64953f72aba1682fc
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-03 14:27:26 -0600

    Problem 15

--- Chinook DB.session.sql
+++ Chinook DB.session.sql
@@ -120,3 +120,13 @@ GROUP BY BillingCountry
 ;
 
 
+-- 15 playlists_track_count.sql: Provide a query that shows the total number of tracks 
+-- in each playlist. The Playlist name should be included on the resulant table.
+SELECT
+    COUNT(TrackId) AS NumberOfTracks,
+    Name
+FROM Playlist
+JOIN
+    PlaylistTrack AS pt ON pt.PlaylistId = Playlist.PlaylistId
+GROUP BY Playlist.PlaylistId
+;
